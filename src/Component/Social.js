e174c7fbdb7808ff9d0c0390bd6feae01261a49d
ManaYupanan <62202448+ManaYupanan@users.noreply.github.com>
2021-02-17 10:40:18 +0700

    update href
    update href Github

--- src/Component/Social.js
+++ src/Component/Social.js
@@ -5,7 +5,7 @@ class Social extends Component{
         return(
             <div className="social">
                 <a href="https://www.facebook.com/profile.php?id=100001981071939" target="_blank" rel="noopender noreferrer" class="fab fa-facebook"></a>
-                <a href="#" target="_blank" rel="noopender noreferrer" class="fab fa-github"></a>
+                <a href="https://github.com/ManaYupanan/my-portfolio.git" target="_blank" rel="noopender noreferrer" class="fab fa-github"></a>
                 <a href="https://www.instagram.com/kerr_snook/" target="_blank" rel="noopender noreferrer" class="fab fa-instagram"></a>
                 
             </div>
